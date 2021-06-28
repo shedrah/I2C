0070c6dd905d780c5b48e0a33193a8cfc546caf6
shedrah <43993174+shedrah@users.noreply.github.com>
2021-06-28 20:17:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,120 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
+#include <i2c.h>
+#include <FS.h>
+#include <stdio.h>
+
+const char *sdFile = "Dane/File.txt";
+char buf[128];
+/*
+uint8 usbRX(uint8 *buf2)
+{
+    uint8 count=0;
+    if(USBUART_1_DataIsReady()!=0){
+        count=USBUART_1_GetAll(buf2);
+        if(count!=0){
+            while(USBUART_1_CDCIsReady()==0);
+            USBUART_1_PutData(buf2,count);
+        }
+    }
+    return count;
+}
 */
-#include <project.h>
-#include <i2c.c>
 int main()
 {
-   
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    CyGlobalIntEnable;
     I2C_1_Start();
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    //UART_1_Start();
+    USBUART_1_Start(USBUART_1_CLASS_DEVICE,USBUART_1_5V_OPERATION);
     
-    //test
-    uint8 deviceID;
-    uint8 CtrlReg1_beforeWrite;
-    uint8 CtrlReg1_afterWrite;
-    uint8 x_MSB, x_LSB, y_MSB, y_LSB, z_MSB, z_LSB;
+    while(!USBUART_1_GetConfiguration());
+    USBUART_1_CDC_Init();
+    
+    accel2_WriteReg(CTRL_REG1, 0b00010000);
+    
+    accel2_WriteReg(CTRL_REG2, 0b00010000);
+    
+    uint8 x_MSB, x_LSB, y_MSB, y_LSB, z_MSB, z_LSB, g_x_MSB, g_x_LSB, g_y_MSB, g_y_LSB, g_z_MSB, g_z_LSB;
+    
+    //uint8 usbRxBufer[1024];
+    //uint8 usbTxBufer[1024];
+     
+    int16_t valX;
+    int16_t valY;
+    int16_t valZ;
+        
+    int16_t g_valX;
+    int16_t g_valY;
+    int16_t g_valZ; 
+        
+     
+    char sdVolName[10];     
+    FS_FILE * pFile;
+    if(!USBUART_1_GetConfiguration())
+    {
+    int SD_name=FS_GetVolumeName(0u, &sdVolName[0], 9u);
+    FS_Init();
+    char plik;
+    FS_MkDir ("Dane");
+    pFile = FS_FOpen(sdFile, "a");
+    }
     
     
-    CtrlReg1_beforeWrite=accel_ReadReg(CTRL_REG1);
-    
-    accel_WriteReg(CTRL_REG1,00010000);
-    
-    CtrlReg1_afterWrite=accel_ReadReg(CTRL_REG1);
-    
-    // koniec testu
     for(;;)
     {
-        deviceID=accel_ReadReg(WHO_AM_I);
+        x_MSB=accel2_ReadReg(OUT_X_MSB);
+        x_LSB=accel2_ReadReg(OUT_X_LSB);
         
-        x_MSB=accel_ReadReg(OUT_X_MSB);
-        x_LSB=accel_ReadReg(OUT_X_LSB);
+        y_MSB=accel2_ReadReg(OUT_Y_MSB);
+        y_LSB=accel2_ReadReg(OUT_Y_LSB);
         
-        y_MSB=accel_ReadReg(OUT_Y_MSB);
-        y_LSB=accel_ReadReg(OUT_Y_LSB);
+        z_MSB=accel2_ReadReg(OUT_Z_MSB);
+        z_LSB=accel2_ReadReg(OUT_Z_LSB);
         
-        z_MSB=accel_ReadReg(OUT_Z_MSB);
-        z_LSB=accel_ReadReg(OUT_Z_LSB);
+        g_x_MSB=accel2_ReadReg(GOUT_X_MSB);
+        g_x_LSB=accel2_ReadReg(GOUT_X_LSB);
+        
+        g_y_MSB=accel2_ReadReg(GOUT_Y_MSB);
+        g_y_LSB=accel2_ReadReg(GOUT_Y_LSB);
+        
+        g_z_MSB=accel2_ReadReg(GOUT_Z_MSB);
+        g_z_LSB=accel2_ReadReg(GOUT_Z_LSB);
+        
+       
+        valX = (x_MSB << 8) | x_LSB;
+        valY = (y_MSB << 8) | y_LSB;
+        valZ = (z_MSB << 8) | z_LSB;
+         
+        g_valX = (g_x_MSB << 8) | g_x_LSB;
+        g_valY = (g_y_MSB << 8) | g_y_LSB;
+        g_valZ = (g_z_MSB << 8) | g_z_LSB;
+        
+        sprintf(buf, "%+d, %+d, %+d, %+d, %+d, %+d\n", valX, valY, valZ,g_valX, g_valY, g_valZ);
+        CyDelay(100);
+        USBUART_1_PutString(buf);
+       
+   
+        /*
+        if(pFile)
+        {
+         FS_Write(pFile, "X: " + valX, 10u);
+         FS_Write(pFile, "Y: " + valY, 10u);
+         FS_Write(pFile, "Z: " + valZ, 10u);
+    
+         FS_Write(pFile, "g_X: " + g_valX, 10u);
+         FS_Write(pFile, "g_Y: " + g_valY, 10u);  
+         FS_Write(pFile, "g_Z: " + g_valZ, 10u);
+        }
+            */
+        
+        
+        CyDelay(100);
+    } 
+    
+    
+    /*
+    FS_FClose(pFile);
+    FS_CopyFile(sdFile, "\\Dane\\acce2.txt");
+    for(;;)
+    {
     }
+    */
 }
-
-/* [] END OF FILE */
